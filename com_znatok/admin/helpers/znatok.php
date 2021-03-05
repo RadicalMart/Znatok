a752eff6d3d949ded70abfc83135b2c5f48d2958
Septdir <septdir@gmail.com>
2021-03-05 16:44:03 +0300

    CleanUp

--- com_znatok/admin/helpers/znatok.php
+++ com_znatok/admin/helpers/znatok.php
@@ -11,11 +11,8 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Helper\ContentHelper;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Plugin\PluginHelper;
-use Joomla\Registry\Registry;
 
 class ZnatokHelper extends ContentHelper
 {
@@ -31,37 +28,5 @@ class ZnatokHelper extends ContentHelper
 		JHtmlSidebar::addEntry(Text::_('COM_ZNATOK_DASHBOARD'),
 			'index.php?option=com_znatok&view=dashboard',
 			$vName == 'dashboard');
-
-//		JHtmlSidebar::addEntry(Text::_('COM_ZNATOK_URLS'),
-//			'index.php?option=com_znatok&view=urls',
-//			$vName == 'urls');
-	}
-
-	/**
-	 * Method to get site check result.
-	 *
-	 * @return array Checks results.
-	 *
-	 * @since  __DEPLOY_VERSION__
-	 */
-	public static function getSiteWarnings()
-	{
-		$result = array();
-
-		// Check sef plugin
-		if ($sef = PluginHelper::getPlugin('system', 'sef'))
-		{
-			if ((new Registry($sef->params))->get('domain'))
-			{
-				$result[] = 'sef_plugin_domain';
-			}
-		}
-
-		// Check content
-		$content = ComponentHelper::getParams('com_content');
-		if (!$content->get('sef_advanced')) $result[] = 'content_sef_advanced';
-		if ($content->get('show_feed_link')) $result[] = 'content_show_feed_link';
-
-		return $result;
 	}
 }
--- com_znatok/admin/views/dashboard/view.html.php
+++ com_znatok/admin/views/dashboard/view.html.php
@@ -11,21 +11,11 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Language\Text;
+use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\View\HtmlView;
-use Joomla\CMS\Toolbar\ToolbarHelper;
 
 class ZnatokViewDashboard extends HtmlView
 {
-	/**
-	 * View sidebar.
-	 *
-	 * @var  string
-	 *
-	 * @since  __DEPLOY_VERSION__
-	 */
-	public $sidebar;
-
 	/**
 	 * Display the view.
 	 *
@@ -39,39 +29,8 @@ class ZnatokViewDashboard extends HtmlView
 	 */
 	public function display($tpl = null)
 	{
-		// Add title and toolbar
-		$this->addToolbar();
-
-		// Prepare sidebar
-		ZnatokHelper::addSubmenu('dashboard');
-		$this->sidebar = JHtmlSidebar::render();
-
-		// Check for errors
-		if (count($errors = $this->get('Errors')))
-		{
-			throw new Exception(implode('\n', $errors), 500);
-		}
+		Factory::getApplication()->redirect('index.php?option=com_config&view=component&component=com_znatok');
 
 		return parent::display($tpl);
-	}
-
-	/**
-	 * Add title and toolbar.
-	 *
-	 * @since  __DEPLOY_VERSION__
-	 */
-	protected function addToolbar()
-	{
-		$canDo = ZnatokHelper::getActions('com_znatok', 'dashboard');
-
-		// Set page title
-		ToolbarHelper::title(Text::_('COM_ZNATOK') . ': ' . Text::_('COM_ZNATOK_DASHBOARD'), 'lamp');
-
-
-		// Add preferences button
-		if ($canDo->get('core.admin') || $canDo->get('core.options'))
-		{
-			ToolbarHelper::preferences('com_znatok');
-		}
 	}
 }
