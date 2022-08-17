d65a0de7c81a28647475238e83312356f00ffee0
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2022-08-17 13:51:00 +0300

    Reset to dev

--- script.php
+++ script.php
@@ -2,7 +2,7 @@
 /*
  * @package     Znatok Package
  * @subpackage  pkg_znatok
- * @version     1.0.1
+ * @version     __DEPLOY_VERSION__
  * @author      Delo Design - delo-design.ru
  * @copyright   Copyright (c) 2021 Delo Design. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
